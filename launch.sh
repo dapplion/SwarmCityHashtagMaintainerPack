28842e01477012885a2bb100f4481e823724d461
dapplion <guillem.lion.benosa@gmail.com>
2019-02-24 17:54:54 +0100

    Add auto setup script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,39 @@
+#!/bin/bash
+
+# Auto-configure the server
+# =========================
+
+# Load previously set envs (if file exists)
+ENV_FILE=".env" && test -f $ENV_FILE && source $ENV_FILE
+
+if [ -z "$PRIVATE_KEY" ]
+then
+    echo "Enter the private key of the faucet account"
+    echo "  i.e. 144a10dfcce26b149372b2d978e61278..."
+    read -p "  Private key: " PRIVATE_KEY
+else
+    echo "Reading previously set PRIVATE_KEY: $PRIVATE_KEY"
+fi
+
+if [ -z "$WEB3_PROVIDER" ]
+then
+    echo "Enter the URL of a kovan node JSON RPC to be used by the faucet. Leave empty to use Infura"
+    echo "  i.e. 'http://87.241.23.5/kovan:8545', 'https://kovan.infura.io/v3/your-token', ... "
+    read -p "  Kovan node JSON RPC URL: " WEB3_PROVIDER
+else
+    echo "Reading previously set WEB3_PROVIDER: $WEB3_PROVIDER"
+fi
+
+# Save envs
+
+cat >$ENV_FILE <<EOL
+PRIVATE_KEY=$PRIVATE_KEY
+WEB3_PROVIDER=$WEB3_PROVIDER
+EOL
+
+# Download repos and launch
+# =========================
+
 rm -rf shortcode/ && git clone https://github.com/dapplion/SwarmCityShortcode.git shortcode
 rm -rf faucet/ && git clone https://github.com/dapplion/SwarmCityFaucet.git faucet
 rm -rf shortener/ && git clone https://github.com/dapplion/SwarmCityShortener.git shortener
